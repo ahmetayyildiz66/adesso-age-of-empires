41814bf5cad58e714b9c20fd5acff02f6a7c92df
ahmetayyildiz66 <ahmetayyildiz66@gmail.com>
2021-03-20 16:20:47 +0300

    Listed units in table

--- src/views/Units.vue
+++ src/views/Units.vue
@@ -1,12 +1,58 @@
 <template>
-  <h1>Units page</h1>
+  <table class="table">
+    <thead>
+    <tr>
+      <th>id</th>
+      <th>name</th>
+      <th>age</th>
+      <th>costs</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(unit, index) in getUnits" :key="unit.id + index">
+      <UnitItem :unit="unit"/>
+    </tr>
+  </tbody>
+  </table>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import UnitItem from './UnitItem.vue';
+
 export default {
   name: 'Units',
+  components: {
+    UnitItem,
+  },
+  computed: {
+    ...mapGetters('units', ['getUnits']),
+  },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.table {
+  width: 80%;
+  border-collapse: collapse;
+  border: 3px solid $color-dark-blue;
+  margin: 1rem 1rem 0 1rem;
+  & th {
+    text-align: center;
+    background-color: $color-dark-blue;
+    color: $color-white;
+    padding: .8rem;
+    min-width: 3rem;
+  }
+
+  & td {
+    text-align: center;
+    padding: .8rem;
+    border-right: 2px solid $table-row-color;
+
+    &:last-child {
+      border-right: none;
+    }
+  }
+}
 </style>
