88a25f730d9d3fed6ce7cd00aed89bfd2a29d497
ahmetayyildiz66 <ahmetayyildiz66@gmail.com>
2021-03-20 14:40:07 +0300

    Basic setup

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img src="../assets/home-image.png" class="home__image" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  &__image {
+    @include absoluteCenter;
+  }
+}
+</style>
